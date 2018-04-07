e512ae5b3b9251e89ab40b58869a3774f0674c5c
Firrero <firrero77@gmail.com>
2018-04-07 13:28:06 +0300

    Added server files

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+const user = require('./routes/user-route');
+
+const app = express()
+
+const PORT = 8081;
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+mongoose.connect('mongodb://admin:admin@ds227199.mlab.com:27199/dashboard');
+
+// further development
+app.post('/verify', (req, res) => {
+    res.send(
+    [{
+        token: "token"
+    }]
+)
+})
+
+app.use('/user', user);
+
+app.listen(PORT, function(){
+    console.log('Server is running on Port',PORT);
+});
---
+++ server/src/routes/user-route.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const jwt = require('jsonwebtoken');
+
+
+/*----- Sign new account ---- */
+router.post('/signup', function(req, res) {
+
+	User.findOne({email: req.body.email})
+		.exec()
+		.then(function(user) {
+			res.status(500).json({
+				error: 'Email is already used',
+				user:user.name
+			});
+		})
+		.catch(error => {
+			bcrypt.hash(req.body.password, 10, function(err, hash){
+				if(err) {
+					return res.status(500).json({
+						error: err
+					});
+				}
+				else {
+					const user = new User({
+						_id: new  mongoose.Types.ObjectId(),
+						email: req.body.email,
+						password: hash
+					});
+
+					user.save().then(function(result) {
+						console.log("result!");
+						res.status(200).json({
+							success: 'New user has been created'
+						});
+					})
+				}
+			});
+		});
+
+});
+
+module.exports = router;
+
+/*----- Sign in the user  ---- */
+router.post('/signin', function(req, res){
+    User.findOne({email: req.body.email})
+        .exec()
+        .then(function(user) {
+            bcrypt.compare(req.body.password, user.password, function(err, result){
+                if(err) {
+                    return res.status(401).json({
+                        error: 'Unauthorized Access'
+                    });
+                }
+                if(result) {
+                    const JWTToken = jwt.sign({
+                            email: user.email,
+                            _id: user._id
+                        },
+                        'secret',
+                        {
+                            expiresIn: '2h'
+                        });
+                    return res.status(200).json({
+                        success: 'Welcome to the JWT Auth',
+                        user:user.email,
+                        token: JWTToken
+                    });
+                }
+                return res.status(401).json({
+                    error: 'Unauthorized Access'
+                });
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Invalid email name"
+            });
+        });
+});
